2e2ba58f7d473afac1c70ac370cd45ac2412f5be
elbeejay <jayaram.hariharan@utexas.edu>
2023-03-15 13:30:35 +0000

    Deploying to gh-pages from @ elbeejay/dataretrieval-1@5a45c761bf7aa367cd1287c4bfe546ca43e1d2ff 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+gcfe78ae',
+    VERSION: '0.1.dev1+g5a45c76',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
